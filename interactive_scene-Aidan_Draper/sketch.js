2c2ccc1cca10ff154aea9bbacc0552a0ffa2400f
Aidan <0802324@sps-student.sk.ca>
2022-09-27 12:16:37 -0600

    created new project separate from the practice

---
+++ interactive_scene-Aidan_Draper/sketch.js
@@ -0,0 +1,15 @@
+// Memory Match
+// Aidan Draper
+// September 27, 2022
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
