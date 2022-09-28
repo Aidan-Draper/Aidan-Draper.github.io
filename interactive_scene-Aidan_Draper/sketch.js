1f4cc9e5bf5457fce245e65cc072d46755e15f86
Aidan <0802324@sps-student.sk.ca>
2022-09-28 11:22:39 -0600

    worked on making card

--- interactive_scene-Aidan_Draper/sketch.js
+++ interactive_scene-Aidan_Draper/sketch.js
@@ -5,11 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let cardImage, scalar, x, y, cardWidth, cardHeight;
+
+function preload(){
+  cardImage = loadImage("card_back_purple.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cardWidth = cardImage.width*scalar;
+  cardHeight = cardImage.height*scalar;
 }
 
 function draw() {
   background(220);
+  drawCard();
+}
+
+function drawCard(){
+
 }
