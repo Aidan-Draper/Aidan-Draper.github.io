0db425eb973d78743edf2301ebc8e7203153c5ce
Aidan <0802324@sps-student.sk.ca>
2022-10-12 12:09:27 -0600

    renamed stuff

---
+++ Final_interactive_scene-Aidan_Draper/sketch.js
@@ -0,0 +1,15 @@
+// Memory match(interactive scene)
+// Aidan Draper
+// October 13, 2022
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
