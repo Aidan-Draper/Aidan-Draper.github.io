a913b2fd72d84c4262e30bdd8797afdd0c0c0ce3
Aidan <0802324@sps-student.sk.ca>
2022-10-31 11:42:08 -0600

    made basics

--- Suduko-Aidan_Draper/sketch.js
+++ Suduko-Aidan_Draper/sketch.js
@@ -1,15 +1,44 @@
-// Project Title
-// Your Name
-// Date
+// Sudoku Generator
+// Aidan Draper
+// October 31, 2022
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+const ROWS = 9;
+const COLS = 9;
+let cellSideLength;
+let unmixedBox = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let sudoku = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  generateRandomBoxes (unmixedBox, sudoku);
+  console.log(sudoku);//checking progress
+  if (width >= height) {
+    cellSideLength = height/ROWS;
+  }
+  else if (height >= width) {
+    cellSideLength = width/COLS;
+  }
 }
 
 function draw() {
   background(220);
+  displayGrid(sudoku);
+}
+
+function generateRandomBoxes (box, grid){
+  box.forEach((element, index) => {
+    grid.push(shuffle(box));
+  });
+}
+
+function displayGrid(grid) {
+  for (let y = 0; y<ROWS; y++) {
+    for (let x = 0; x<COLS; x++) {
+      rect(x*cellSideLength, y*cellSideLength, cellSideLength, cellSideLength);
+      text(grid[y][x], x*cellSideLength, y*cellSideLength);
+    }
+  }
 }
