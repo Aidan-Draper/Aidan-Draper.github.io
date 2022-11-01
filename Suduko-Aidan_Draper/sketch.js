9bef966c61f665ca95ecc56ca8fa4ed783d4da97
Aidan <0802324@sps-student.sk.ca>
2022-11-01 12:46:23 -0600

    tried something that didn't work

--- Suduko-Aidan_Draper/sketch.js
+++ Suduko-Aidan_Draper/sketch.js
@@ -5,22 +5,22 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const ROWS = 9;
-const COLS = 9;
 let cellSideLength;
 let unmixedBox = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let sudoku = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  generateRandomBoxes (unmixedBox, sudoku);
   textAlign(CENTER, CENTER);
+
+  generateRandomRows(sudoku, unmixedBox);
   console.log(sudoku);//checking progress
+  
   if (width >= height) {
-    cellSideLength = height/ROWS;
+    cellSideLength = height/sudoku.length;
   }
   else if (height >= width) {
-    cellSideLength = width/COLS;
+    cellSideLength = width/sudoku.length;
   }
 }
 
@@ -29,19 +29,30 @@ function draw() {
   displayGrid(sudoku);
 }
 
-function generateRandomBoxes (box, grid){
-  box.forEach((element, index) => {
-    grid.push(shuffle(box));
+function generateRandomRows(grid, row){
+  row.forEach((element, index) => {
+    grid.push(shuffle(row));
   });
 }
 
-//displaying each list in a row
 function displayGrid(grid) {
-  for (let y = 0; y<ROWS; y++) {
-    for (let x = 0; x<COLS; x++) {
+  for (let y = 0; y<grid.length; y++) {
+    for (let x = 0; x<grid.length; x++) {
       rect(x*cellSideLength, y*cellSideLength, cellSideLength, cellSideLength);
       textSize(cellSideLength/4);
       text(grid[y][x], x*cellSideLength + cellSideLength/2, y*cellSideLength +cellSideLength/2);
+
+      // let tempArray = [];
+      // if(y%2){
+      // //look at all cells surounding the middle of the array
+      //   for(let i = y-1; i<= y+1; i++){
+      //     for(let  j= x-1; j<= x+1; j++){
+      //       tempArray.push(grid[i][j]);
+      //     }
+      //   }
+      //   console.log(tempArray);
+      // }
     }
   }
 }
+
